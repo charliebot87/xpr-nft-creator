f152fef0e88c99cb27bee369e6054d97fcc1a06e
charliebot87 <charliebot87@users.noreply.github.com>
2026-03-15 14:19:48 +1300

    revert: clean next.config, Anchor only - wallet must work

--- next.config.js
+++ next.config.js
@@ -5,41 +5,6 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   distDir: '.next',
-  experimental: {
-    esmExternals: 'loose',
-  },
-  transpilePackages: [
-    '@proton/ual-webauth',
-    '@proton/web-sdk',
-    '@proton/link',
-    '@proton/browser-transport',
-    '@proton/signing-request',
-  ],
-  // Don't transpile ual-anchor (it uses node 'net' module)
-  // but we still need it at runtime
-  webpack: (config, { isServer }) => {
-    // Fix @proton/link default export issue
-    // The ESM version exports `Link as default` but the CJS doesn't
-    config.resolve.alias = {
-      ...config.resolve.alias,
-    };
-
-    // Force webpack to use the ESM version of @proton/link
-    config.resolve.mainFields = ['module', 'main'];
-
-    // Polyfill node modules for browser
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-        fs: false,
-        dns: false,
-      };
-    }
-
-    return config;
-  },
   images: {
     domains: [
       'ipfs.io',
