668819a07e14d2ec79ad80c41c1c83835f409a57
charliebot87 <charliebot87@users.noreply.github.com>
2026-03-15 13:30:48 +1300

    XPR only: custom logo, favicon, Anchor wallet, clean config

--- src/configs/chainsConfig.ts
+++ src/configs/chainsConfig.ts
@@ -1,45 +1,14 @@
 import { Anchor } from 'ual-anchor';
 
-let ProtonWebAuth: any = null;
-try {
-  ProtonWebAuth = require('@proton/ual-webauth').ProtonWebAuth;
-} catch (e) {
-  // WebAuth not available
-}
-
-const xprAuth = ProtonWebAuth ? [ProtonWebAuth, Anchor] : [Anchor];
-
 module.exports = {
   xpr: {
     name: 'XPR Network',
-    imageUrl: 'https://xprnetwork.org/images/xpr-icon.png',
-    authenticators: xprAuth,
+    imageUrl: '/xpr-icon.png',
+    authenticators: [Anchor],
     aaEndpoint: process.env.NEXT_PUBLIC_XPR_MAINNET_AA_ENDPOINT,
     chainId: process.env.NEXT_PUBLIC_XPR_MAINNET_CHAIN_ID,
     protocol: process.env.NEXT_PUBLIC_XPR_MAINNET_PROTOCOL,
     host: process.env.NEXT_PUBLIC_XPR_MAINNET_HOST,
     port: process.env.NEXT_PUBLIC_XPR_MAINNET_PORT,
-  },
-
-  eos: {
-    name: 'EOS',
-    imageUrl: 'https://bloks.io/img/chains/eos.png',
-    authenticators: [Anchor],
-    aaEndpoint: process.env.NEXT_PUBLIC_EOS_MAINNET_AA_ENDPOINT,
-    chainId: process.env.NEXT_PUBLIC_EOS_MAINNET_CHAIN_ID,
-    protocol: process.env.NEXT_PUBLIC_EOS_MAINNET_PROTOCOL,
-    host: process.env.NEXT_PUBLIC_EOS_MAINNET_HOST,
-    port: process.env.NEXT_PUBLIC_EOS_MAINNET_PORT,
-  },
-
-  wax: {
-    name: 'WAX',
-    imageUrl: 'https://wax.bloks.io/img/chains/wax.png',
-    authenticators: [Anchor],
-    aaEndpoint: process.env.NEXT_PUBLIC_WAX_MAINNET_AA_ENDPOINT,
-    chainId: process.env.NEXT_PUBLIC_WAX_MAINNET_CHAIN_ID,
-    protocol: process.env.NEXT_PUBLIC_WAX_MAINNET_PROTOCOL,
-    host: process.env.NEXT_PUBLIC_WAX_MAINNET_HOST,
-    port: process.env.NEXT_PUBLIC_WAX_MAINNET_PORT,
   },
 };
