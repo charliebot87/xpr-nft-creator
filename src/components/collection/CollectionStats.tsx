4a1d6d09b304ecb4b18f12fe74942b8c84c30a56
charliebot87 <charliebot87@users.noreply.github.com>
2026-03-16 09:15:21 +1300

    fix: replace Twitter bird with X logo

--- src/components/collection/CollectionStats.tsx
+++ src/components/collection/CollectionStats.tsx
@@ -1,6 +1,5 @@
 import { collectionTabs } from '@utils/collectionTabs';
 import {
-  TwitterLogo,
   MediumLogo,
   FacebookLogo,
   GithubLogo,
@@ -8,6 +7,14 @@ import {
   YoutubeLogo,
   TelegramLogo,
 } from 'phosphor-react';
+
+function XIcon({ size = 24 }: { size?: number }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor">
+      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+    </svg>
+  );
+}
 import { CollectionProps } from '@services/collection/getCollectionService';
 
 interface CollectionStatsProps {
@@ -54,7 +61,7 @@ export function CollectionStats({ stats, collection }: CollectionStatsProps) {
   function handleSocialIcon(social) {
     switch (social) {
       case 'twitter':
-        return <TwitterLogo size={24} />;
+        return <XIcon size={24} />;
         break;
       case 'facebook':
         return <FacebookLogo size={24} />;
